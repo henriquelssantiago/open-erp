0fa1859e225f0e5b4e5ab633dcad86f600d0d8e2
Henrique Santiago <suporte8@rcostaconsult.com.br>
2022-08-30 12:22:29 -0300

    Alterando api /

--- open-erp-api/src/main/java/br/com/openerp/api/OpenERPApiController.java
+++ open-erp-api/src/main/java/br/com/openerp/api/OpenERPApiController.java
@@ -1,12 +1,18 @@
 package br.com.openerp.api;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenERPApiController {
     @GetMapping
-    public String get() {
-        return "Server is up";
+    public Map<String, String> get() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "ok");
+        response.put("message", "Macho vei, finalmente!! kkk");
+        return response;
     }
 }
